64f438b736bd129f204649629166fe66be9b536c
Anthony Gégo <anthonygego@hotmail.com>
2016-06-01 15:18:00 +0200

    Plot bar and groups bar with intelligent axis

---
+++ make_plot.m
@@ -0,0 +1,29 @@
+function plt = make_plot(map_list, legend_list, compstr)
+
+num_groups=length(map_list);
+len=length(map_list{1});
+
+% Prepare value vector
+values = zeros(num_groups,len);
+for g=1:num_groups
+    for k=1:len
+        map = map_list{g}{k};
+        values(g,k) = map(compstr);
+    end
+end
+
+% Compute interesting axis values
+p = values';
+dvalues = p(:)';
+meanval=mean(dvalues);
+stddev=std(dvalues);
+
+
+% Draw graph
+bar(values);
+axis([0.5 len+0.5 meanval-3*stddev meanval+3*stddev]);
+xticklabel_rotate(1:len,45,legend_list{1},'interpreter','none')
+plt=gca;
+
+end
+
