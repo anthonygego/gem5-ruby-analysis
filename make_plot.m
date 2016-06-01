86badfa0d184928c7b84c8ee16ae41ea301d5af5
Anthony Gégo <anthonygego@hotmail.com>
2016-06-01 17:03:00 +0200

    Plot with legend

--- make_plot.m
+++ make_plot.m
@@ -1,4 +1,4 @@
-function plt = make_plot(map_list, legend_list, compstr)
+function plt = make_plot(the_title, map_list, legend_list, compstr)
 
 num_groups=length(map_list);
 len=length(map_list{1});
@@ -23,6 +23,13 @@ stddev=std(dvalues);
 bar(values);
 axis([0.5 len+0.5 meanval-3*stddev meanval+3*stddev]);
 xticklabel_rotate(1:len,45,legend_list{1},'interpreter','none')
+
+if num_groups > 1
+    legend(legend_list{2})
+end
+
+title(the_title);
+
 plt=gca;
 
 end
