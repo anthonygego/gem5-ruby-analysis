7fb5a99e000d8d81de2de7c712f0f8d06d454c4d
Anthony Gégo <anthonygego@hotmail.com>
2016-06-01 14:42:00 +0200

    function get hashmap

---
+++ get_stat_hashmap.m
@@ -0,0 +1,38 @@
+function arr = get_stat_hashmap(filename)
+    fid = fopen(filename);
+    arr = containers.Map;
+
+    line = fgetl(fid);
+    while ischar(line)
+
+        if (strcmp(line, '---------- Begin Simulation Statistics ----------'))
+            
+        elseif (strcmp(line, '---------- End Simulation Statistics   ----------'))
+            break;
+        end
+
+        arr_line = [];
+
+        split_line = regexp(line,'\s','split');
+        i=0;
+        for sp=split_line
+            token=sp{1};
+            if strcmp(token, '')
+            elseif strcmp(token, '|')   
+            elseif strcmp(token, '#')
+                break;
+            elseif (~isempty(strfind(token, '%')))
+            else
+                i=i+1;
+                arr_line{i} = token;
+            end
+        end
+
+        if (~isempty(arr_line))
+            %disp(arr_line)
+            arr(arr_line{1}) = str2double(arr_line(2:end));
+        end
+
+        line = fgetl(fid);
+    end
+end
--- make_graphs.m
+++ make_graphs.m
@@ -1,46 +1,13 @@
-function testa = make_graphs()
+function make_graphs()
 
-fid = fopen('../lelec2990-sim/blackscholes/X86/cacheline_size/64b/stats.txt');
-arr = containers.Map;
+map = get_stat_hashmap('../lelec2990-sim/blackscholes/X86_MOESI_CMP_directory/cacheline_size/64b/stats.txt');
 
-line = fgetl(fid);
-while ischar(line)
-    
-    if (strcmp(line, '---------- Begin Simulation Statistics ----------'))
-        display('lol');
-    elseif (strcmp(line, '---------- End Simulation Statistics   ----------'))
-        break;
-    end
-    
-    arr_line = [];
-    
-    split_line = regexp(line,'\s','split');
-    i=0;
-    for sp=split_line
-        token=sp{1};
-        if strcmp(token, '')
-        elseif strcmp(token, '|')   
-        elseif strcmp(token, '#')
-            break;
-        elseif (~isempty(strfind(token, '%')))
-        else
-            i=i+1;
-            arr_line{i} = token;
-        end
-    end
-    
-    if (~isempty(arr_line))
-        %disp(arr_line)
-        arr(arr_line{1}) = str2double(arr_line(2:end));
-    end
-    
-    line = fgetl(fid);
-end
-
-testa= arr('system.ruby.L1Cache_Controller.IM.Data')
+disp(map('sim_seconds'))
 
 %list_benchmarks()
 end
+
+
 
 function blist = list_benchmarks()
 
