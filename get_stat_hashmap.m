bed23f5295c0be541f295feeb6d6840e2f5cd40d
Anthony Gégo <anthonygego@hotmail.com>
2016-06-06 12:01:54 +0200

    Cache for matfiles

--- get_stat_hashmap.m
+++ get_stat_hashmap.m
@@ -1,38 +1,49 @@
 function arr = get_stat_hashmap(filename)
-    fid = fopen(filename);
-    arr = containers.Map;
-
-    line = fgetl(fid);
-    while ischar(line)
-
-        if (strcmp(line, '---------- Begin Simulation Statistics ----------'))
-            
-        elseif (strcmp(line, '---------- End Simulation Statistics   ----------'))
-            break;
-        end
-
-        arr_line = [];
-
-        split_line = regexp(line,'\s','split');
-        i=0;
-        for sp=split_line
-            token=sp{1};
-            if strcmp(token, '')
-            elseif strcmp(token, '|')   
-            elseif strcmp(token, '#')
-                break;
-            elseif (~isempty(strfind(token, '%')))
-            else
-                i=i+1;
-                arr_line{i} = token;
-            end
-        end
-
-        if (~isempty(arr_line))
-            %disp(arr_line)
-            arr(arr_line{1}) = str2double(arr_line(2:end));
-        end
+    % Check for mat file
+    [pathstr,~,~] = fileparts(filename);
+    matfile = [pathstr '/stats.mat'];
+    
+    if(exist(matfile, 'file'))
+        load(matfile, 'arr');
+    else
+        % Open the stats file
+        fid = fopen(filename);
+        arr = containers.Map;
 
         line = fgetl(fid);
+        while ischar(line)
+
+            if (strcmp(line, '---------- Begin Simulation Statistics ----------'))
+
+            elseif (strcmp(line, '---------- End Simulation Statistics   ----------'))
+                break;
+            end
+
+            arr_line = [];
+
+            split_line = regexp(line,'\s','split');
+            i=0;
+            for sp=split_line
+                token=sp{1};
+                if strcmp(token, '')
+                elseif strcmp(token, '|')   
+                elseif strcmp(token, '#')
+                    break;
+                elseif (~isempty(strfind(token, '%')))
+                else
+                    i=i+1;
+                    arr_line{i} = token;
+                end
+            end
+
+            if (~isempty(arr_line))
+                arr(arr_line{1}) = str2double(arr_line(2:end));
+            end
+
+            line = fgetl(fid);
+        end
+
+        % Save the mat file
+        save(matfile, 'arr');
     end
 end
--- make_graphs.m
+++ make_graphs.m
@@ -8,10 +8,10 @@ function make_graphs()
 % 
 % test = make_plot('lol', maps, {{'prot1', 'prot2'},{'var1','var2'}}, 'sim_seconds');
 
-maps{1}{1} = get_stat_hashmap('../lelec2990-sim/swaptions/X86/cacheline_size/64b/stats.txt');
-maps{1}{2} = get_stat_hashmap('../lelec2990-sim/swaptions/X86_MESI_Two_Level/cacheline_size/64b/stats.txt');
-maps{1}{3} = get_stat_hashmap('../lelec2990-sim/swaptions/X86_MESI_Three_Level/cacheline_size/64b/stats.txt');
-maps{1}{4} = get_stat_hashmap('../lelec2990-sim/swaptions/X86_MOESI_hammer/cacheline_size/64b/stats.txt');
+maps{1}{1} = get_stat_hashmap('../lelec2990-sim/blackscholes/X86/cacheline_size/64b/stats.txt');
+maps{1}{2} = get_stat_hashmap('../lelec2990-sim/blackscholes/X86_MESI_Two_Level/cacheline_size/64b/stats.txt');
+maps{1}{3} = get_stat_hashmap('../lelec2990-sim/blackscholes/X86_MESI_Three_Level/cacheline_size/64b/stats.txt');
+maps{1}{4} = get_stat_hashmap('../lelec2990-sim/blackscholes/X86_MOESI_hammer/cacheline_size/64b/stats.txt');
 
 make_plot('lol', maps, {{'mi', 'mesi2','mesi3','moesih'}}, 'sim_seconds');
 
